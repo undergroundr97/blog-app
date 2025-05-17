287578c4cd9f3d71d02789ead790ab9d46a14f05
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2025-05-17 02:21:41 -0300

    pushing model view controller following rails guide

---
+++ db/migrate/20250517045607_create_articles.rb
@@ -0,0 +1,11 @@
+class CreateArticles < ActiveRecord::Migration[8.0]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :body
+      t.string :rito
+
+      t.timestamps
+    end
+  end
+end
